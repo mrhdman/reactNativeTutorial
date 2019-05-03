0300b68d37849b60266f720eb9c4774ad25d2c7c
mrhdman <mattrhavins@gmail.com>
2019-05-03 18:12:04 -0400

    Props

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, AppRegistry, Image} from 'react-native';
 
+// export default class Bananas extends Component {
+//   render() {
+//     let pic = {
+//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     };
+//     return (
+//       <Image source={pic} style={{width: 200, height: 200}}/>
+//     );
+//   }
+// }
 
-export default class App extends Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello Oxford, Mississippi.</Text>
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
       </View>
     );
   }
 }
+
+export default class CitiesGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name='Oxford' />
+        <Greeting name='Austin' />
+        <Greeting name='Atlanta' />
+      </View>
+    );
+  }
+}
+
+
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//         <Text>Hello Oxford, Mississippi.</Text>
+//       </View>
+//     );
+//   }
+// }
 
