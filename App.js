00516de901ba607e9cfc0ce9b5c8326ef92fdfb8
mrhdman <mattrhavins@gmail.com>
2019-05-04 11:21:09 -0400

    Layout with Flexbox: Flex Direction

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 1.25, backgroundColor: 'red'}} />
-        <View style={{flex: 8, backgroundColor: 'yellow'}} />
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 100, height: 500, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 500, backgroundColor: 'red'}} />
+        <View style={{width: 100, height: 500, backgroundColor: 'blue'}} />
       </View>
-    );
+    )
   }
 }
