a06d2218ffbf6282c5178def31193ed917b97d1c
mrhdman <mattrhavins@gmail.com>
2019-05-04 11:17:00 -0400

    Height and Width: Flex Dimensions

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class FlexDimensionsBasics extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 1000, height: 50, backgroundColor: 'red'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 1.25, backgroundColor: 'red'}} />
+        <View style={{flex: 8, backgroundColor: 'yellow'}} />
       </View>
     );
   }
