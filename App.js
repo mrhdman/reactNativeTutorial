e5172d575bed16c1673c3255366c1d0f6b2c8487
mrhdman <mattrhavins@gmail.com>
2019-05-04 19:05:26 -0400

    Handling Touches: Button Basics

--- App.js
+++ App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''}
+export default class ButtonBasics extends Component {
+  OxfordButton() {
+    Alert.alert('Oxford, Mississippi.')
+  }
+  BornButton() {
+    Alert.alert('Naples, Florida.')
+  }
+  NowButton() {
+    Alert.alert('Atlanta, Georgia.')
+  }
+  TexasButton() {
+    Alert.alert('Austin, Texas.')
   }
 
   render() {
-    return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <TextInput
-          style={{flex: 9, fontSize: 20, paddingLeft: 10}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
+    return(
+      <View style={StyleSheet.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this.OxfordButton}
+            title="Where am I from?"
           />
-          <Text style={{paddingLeft: 10, fontSize: 22}}>
-            {this.state.text.split(' ').map((word) => word && 'pizza').join(' ')}
-          </Text>
-          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button 
+            onPress={this.BornButton}
+            title="Where was I born?"
+            color="red"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this.NowButton}
+            title="Where do I live now?"
+          />
+          <Button
+            onPress={this.TexasButton}
+            title="Where did I live in Texas?"
+            color="orange"
+          />
+        </View>
       </View>
-    );
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  }
+});
