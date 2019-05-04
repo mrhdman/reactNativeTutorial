1d35c774abb2a2cbcdfa10ef622d458b9e7c2e85
mrhdman <mattrhavins@gmail.com>
2019-05-04 11:10:26 -0400

    Height and Width: Fixed Dimensions

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
-export default class LotsOfStyles extends Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.red}>Red</Text>
-        <Text style={styles.bigBlue}>just bigBlue</Text>
-        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+        <View style={{width: 1000, height: 50, backgroundColor: 'red'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
