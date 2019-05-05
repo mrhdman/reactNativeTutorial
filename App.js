5d01b7687a0fe91d3957bfca265837622e6d8b83
mrhdman <mattrhavins@gmail.com>
2019-05-04 20:09:36 -0400

    Using List Views

--- App.js
+++ App.js
@@ -1,42 +1,46 @@
 import React, { Component } from 'react';
-import { AppRegistry, ScrollView, Image, Text } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+export default class FlatListBasics extends Component {
   render() {
-      return (
-        <ScrollView>
-          <Text style={{fontSize:96}}>Scroll me please.</Text>
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>If you like</Text>
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>Scrolling down</Text>
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>What's the best</Text>
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>Town around?</Text>
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg/250px-Lafayette_Co_Mississippi_courthouse_during_Double_Decker_Festival.jpg", width: 64, height: 64}} />
-          <Text style={{fontSize:80}}>Oxford, Mississippi.</Text>
-        </ScrollView>
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'O'},
+            {key: 'OX'},
+            {key: 'OXF'},
+            {key: 'OXFO'},
+            {key: 'OXFOR'},
+            {key: 'OXFORD'},
+            {key: 'OXFORD,'},
+            {key: 'OXFORD,M'},
+            {key: 'OXFORD,Mi'},
+            {key: 'OXFORD,Mis'},
+            {key: 'OXFORD,Miss'},
+            {key: 'OXFORD,Missi'},
+            {key: 'OXFORD,Missis'},
+            {key: 'OXFORD,Mississ'},
+            {key: 'OXFORD,Mississi'},
+            {key: 'OXFORD,Mississip'},
+            {key: 'OXFORD,Mississipp'},
+            {key: 'OXFORD,Mississippi'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 30,
+    height: 44,
+  },
+})
